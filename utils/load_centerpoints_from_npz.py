a6a4105f2e036e5a0aef8b3cbd7d04c4849d0bcf
Ilmari Tarpila <ilmari@tarpila.fi>
2023-12-29 14:59:44 +0200

    refactor: remove direct bounding box loading

--- utils/load_centerpoints_from_npz.py
+++ utils/load_centerpoints_from_npz.py
@@ -1,34 +1,35 @@
 import numpy as np
 
-def load_centerpoints_from_npz(npz_file_path):
+def load_centerpoints_from_npz(npz_file_paths):
     """
     Load centerpoints and their corresponding class labels from an NPZ file.
 
     Args:
-        npz_file_path (str): Path to the NPZ file containing the centerpoint data.
+        npz_file_path (list): List of paths to the NPZ files containing the centerpoint data.
 
     Returns:
         dict: Dictionary containing centerpoints keyed by class labels.
     """
-    # Load the NPZ file
-    data = np.load(npz_file_path, allow_pickle=True)
-
-    # Extract the classes and centerpoints arrays
-    classes = data['classes']
-    centerpoints = data['centerpoints']
-
-    # Group centerpoints by class
     centerpoints_dict = {}
-    for class_label, point in zip(classes, centerpoints):
-        if class_label not in centerpoints_dict:
-            centerpoints_dict[class_label] = []
-        centerpoints_dict[class_label].append(tuple(point))
+
+    # Load the NPZ files
+    for file_path in npz_file_paths:
+        with np.load(file_path, allow_pickle=True) as data:
+            # Extract the classes and centerpoints arrays
+            classes = data['classes']
+            centerpoints = data['centerpoints']
+
+            # Group centerpoints by class
+            for class_label, point in zip(classes, centerpoints):
+                if class_label not in centerpoints_dict:
+                    centerpoints_dict[class_label] = []
+                centerpoints_dict[class_label].append(tuple(point))
 
     return centerpoints_dict
 
 def main():
-    npz_file_path = 'centerpoints.npz'  # Replace with your .npz file path
-    centerpoints_data = load_centerpoints_from_npz(npz_file_path)
+    npz_file_paths = ['data/centerpoints.npz']  # Replace with your .npz file path
+    centerpoints_data = load_centerpoints_from_npz(npz_file_paths)
     print(centerpoints_data[0][:10])
 
 if __name__ == "__main__":
