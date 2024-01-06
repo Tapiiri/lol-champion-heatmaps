a9f928b57d6484ef650ac07155f69bccf7a8c443
Ilmari Tarpila <ilmari@tarpila.fi>
2024-01-06 14:56:01 +0200

    refactor: move save_centerpoints_as_npz to utils

--- convert_deep_league_to_centerpoints.py
+++ convert_deep_league_to_centerpoints.py
@@ -1,34 +1,8 @@
-import numpy as np
 import argparse
-import os
 from utils.deep_league_utils.convert_to_centerpoints import convert_to_centerpoints
 from utils.get_file_paths import get_file_paths
 from utils.deep_league_utils.load_and_combine_npz_bounding_boxes import load_and_combine_npz_bounding_boxes
-
-def save_centerpoints_as_npz(centerpoints_dict, output_file):
-    """
-    Save the centerpoints and their corresponding class labels as an NPZ file.
-
-    Args:
-        centerpoints_dict (dict): Dictionary containing centerpoints keyed by class labels.
-        output_file (str): Path to the output NPZ file.
-    """
-    # Flatten the dictionary into lists of classes and centerpoints
-    classes = []
-    points = []
-    for class_label, centerpoints in centerpoints_dict.items():
-        for point in centerpoints:
-            classes.append(class_label)
-            points.append(point)
-
-    # Convert lists to numpy arrays
-    classes_array = np.array(classes)
-    points_array = np.array(points)
-
-    # Save the arrays as an NPZ file
-    np.savez(output_file, classes=classes_array, centerpoints=points_array)
-    print(f"Saved {len(points)} centerpoints to {output_file}")
-    print(points[:20])
+from utils.save_centerpoints_as_npz import save_centerpoints_as_npz
 
 def main():
     parser = argparse.ArgumentParser(description='Overlay heatmap on map and save as an image.')
