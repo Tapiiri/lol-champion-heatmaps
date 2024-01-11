a785db525b85fd8ace4c99cdebd47b3559762fe8
Ilmari Tarpila <ilmari@tarpila.fi>
2024-01-11 17:36:43 +0200

    fix: file path reading

--- utils/get_file_paths.py
+++ utils/get_file_paths.py
@@ -14,17 +14,21 @@ def get_file_paths(file_names_arg, target_file_type):
         list: A list of target type file paths.
     """
     # If the file_names_arg is a filename, read the file and get a list of file names
+    print("Reading file names...")
     first_file = file_names_arg[0]
-    if len(file_names_arg) == 1 and os.path.isfile(first_file) and not f"{target_file_type}" in first_file:
+    if len(file_names_arg) == 1 and os.path.isfile(first_file) and not f".{target_file_type}" in first_file:
         with open(first_file, 'r') as f:
             file_paths = [line.strip() for line in f.readlines()]
+        print(f"Found {len(file_paths)} files in {first_file}")
     else:
         # Otherwise, assume it's already a list of file names
+        print("No file names file found. Assuming file_names_arg is a list of file names.")
         file_paths = file_names_arg
 
     # Confirm that all file paths have the same ending
-    if not all([f".{target_file_type}" == file_paths[-(len(target_file_type)+1):] for file_paths in file_paths]):
-        raise ValueError(f"Input files not of expected type: should be {target_file_type}")
+    mismatched_file_paths = [f for f in file_paths if not f".{target_file_type}" == f[-(len(target_file_type)+1):]]
+    if not mismatched_file_paths == []:
+        raise ValueError(f"Input files not of expected type: should be {target_file_type}: {mismatched_file_paths[0]} not of type {target_file_type}")
     
     # Check if file paths exist
     file_paths = [f for f in file_paths if os.path.isfile(f)]
